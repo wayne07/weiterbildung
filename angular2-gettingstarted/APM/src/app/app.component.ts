4139a225b64f2da5d94384ea31c44dbe55cb704e
juergen seliger <juergen.seliger@gmail.com>
2017-07-12 18:13:18 +0200

    app läuft initial

---
+++ angular2-gettingstarted/APM/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <div><h1>{{pageTitle}}</h1>
+             <div>My first component</div>
+        </div>
+    `
+})
+export class AppComponent {
+    pageTitle: string = 'Acme Produkt Management by JS';
+ }
