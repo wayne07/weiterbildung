650c0613f5c746b3d4b2c98e7fad896c2c625c8d
juergen seliger <juergen.seliger@gmail.com>
2017-07-19 21:38:00 +0200

    abschluss 19072017

--- angular2-gettingstarted/APM/src/app/app.module.js
+++ angular2-gettingstarted/APM/src/app/app.module.js
@@ -8,9 +8,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
+var http_1 = require("@angular/http");
 var app_component_1 = require("./app.component");
 var product_list_component_1 = require("./products/product-list.component");
 var product_filter_pipe_1 = require("./products/product-filter.pipe");
+var star_component_1 = require("./shared/star.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -19,11 +21,14 @@ var AppModule = (function () {
 AppModule = __decorate([
     core_1.NgModule({
         imports: [platform_browser_1.BrowserModule,
-            forms_1.FormsModule
+            forms_1.FormsModule,
+            http_1.HttpModule
         ],
         declarations: [app_component_1.AppComponent,
             product_list_component_1.ProductListComponent,
-            product_filter_pipe_1.ProductFilterPipe],
+            product_filter_pipe_1.ProductFilterPipe,
+            star_component_1.StarComponent
+        ],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
--- angular2-gettingstarted/APM/src/app/app.module.ts
+++ angular2-gettingstarted/APM/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
-import { ProductListComponent } from './products/product-list.component'
-import { ProductFilterPipe } from './products/product-filter.pipe'
+import { ProductListComponent } from './products/product-list.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   imports: [ BrowserModule,
-             FormsModule
+             FormsModule,
+             HttpModule
            ],
   declarations: [ AppComponent, 
                   ProductListComponent,
-                  ProductFilterPipe ],
+                  ProductFilterPipe,
+                  StarComponent 
+                ],
 
   bootstrap: [ AppComponent ]
 })
