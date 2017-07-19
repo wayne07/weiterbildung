798dfecefd71510fffbfc737bdcb35aef0643e36
juergen seliger <juergen.seliger@gmail.com>
2017-07-19 17:12:33 +0200

    interfaces, custom pipe ...

--- angular2-gettingstarted/APM/src/app/products/product-list.component.js
+++ angular2-gettingstarted/APM/src/app/products/product-list.component.js
@@ -9,6 +9,10 @@ var core_1 = require("@angular/core");
 var ProductListComponent = (function () {
     function ProductListComponent() {
         this.pageTitle = 'Product List';
+        this.imageWidth = 50;
+        this.imageMargin = 2;
+        this.showImage = false;
+        this.listFilter = 'cart';
         this.products = [
             {
                 "productId": 1,
@@ -32,12 +36,20 @@ var ProductListComponent = (function () {
             }
         ];
     }
+    ProductListComponent.prototype.toggleImage = function () {
+        this.showImage = !this.showImage;
+    };
+    ProductListComponent.prototype.ngOnInit = function () {
+        console.log('In OnInit');
+    };
     return ProductListComponent;
 }());
 ProductListComponent = __decorate([
     core_1.Component({
         selector: 'pm-products',
-        templateUrl: 'app/products/product-list.component.html'
+        moduleId: module.id,
+        templateUrl: 'product-list.component.html',
+        styleUrls: ['product-list.component.css']
     })
 ], ProductListComponent);
 exports.ProductListComponent = ProductListComponent;
