7b4aa85371b95f4369cec69438226ff5710bb03b
juergen seliger <juergen.seliger@gmail.com>
2017-09-06 16:57:52 +0200

    spring boot project initial hinzugefügt

---
+++ dasboot/src/main/java/com/boot/controller/ShipwreckController.java
@@ -0,0 +1,37 @@
+package com.boot.controller;
+
+import com.boot.model.Shipwreck;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1")
+public class ShipwreckController {
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
+    public List<Shipwreck> list() {
+        return ShipwreckStub.list();
+    }
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.POST)
+    public Shipwreck create(@RequestBody Shipwreck wreck) {
+        return ShipwreckStub.create(wreck);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
+    public Shipwreck get(@PathVariable Long id) {
+        return ShipwreckStub.get(id);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.PUT)
+    public Shipwreck update(@PathVariable Long id, @RequestBody Shipwreck wreck) {
+        return ShipwreckStub.update(id, wreck);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.DELETE)
+    public Shipwreck delete(@PathVariable Long id) {
+        return ShipwreckStub.delete(id);
+    }
+
+}
