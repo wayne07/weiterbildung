77122f927a1b024c18c9175834c2683acb48227f
Juergen Seliger <juergen.seliger@crafting-it.de>
2017-09-30 15:38:18 +0200

    navdemo init

---
+++ navdemo/src/main/java/de/seliger/NavDemo.java
@@ -0,0 +1,13 @@
+package de.seliger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavDemo.class, args);
+    }
+
+}
